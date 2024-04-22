27173ce686a8a63f072b6f60905dce312ed50bc4
Slobodan Barisic <slobodan.barisic@zooplus.com>
2024-04-22 23:38:21 +0200

    ojkbocind

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Work in progress</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -26,7 +18,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Test, Test
       </p>
     </>
   )
