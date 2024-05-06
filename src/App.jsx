57e849c8a172cf637ebe381ede2d745fc78297e1
Slobodan Barisic <slobodan.barisic@zooplus.com>
2024-05-06 21:40:13 +0200

    navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./App.module.css"
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Work in progress</h1>
-    </>
-  )
-}
+  <div className={styles.App}>
+    <Navbar/>
+  </div>
+)}
 
-export default App
+export default App;
